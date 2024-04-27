4118afd7e92163ca2efde25aef3c2cd9601ffa39
Hari <hari.choudhary1905@gmail.com>
2024-04-27 22:21:18 +0530

    message

--- app/src/main/java/com/app/ecarepro/ui/dashbord/DashboardFragment.kt
+++ app/src/main/java/com/app/ecarepro/ui/dashbord/DashboardFragment.kt
@@ -9,18 +9,29 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.airbnb.epoxy.EpoxyController
 import com.app.ecarepro.R
+import com.app.ecarepro.data.network.model.AdmissionComparison
 import com.app.ecarepro.data.network.model.BankBalance
+import com.app.ecarepro.data.network.model.BirthDayCard
 import com.app.ecarepro.data.network.model.CollectionModeWise
+import com.app.ecarepro.data.network.model.DataValue
 import com.app.ecarepro.data.network.model.FeeCollection
 import com.app.ecarepro.data.network.model.FeeDefaulter
+import com.app.ecarepro.data.network.model.LibraryDetails
 import com.app.ecarepro.data.network.model.StaffAttendance
+import com.app.ecarepro.data.network.model.StatusWiseStatistics
 import com.app.ecarepro.data.network.model.UserDashboardDto
 import com.app.ecarepro.databinding.FragmentDashboardBinding
 import com.app.ecarepro.todayModeWiseCollectionCard
+import com.app.ecarepro.ui.dashbord.model.AdmissionComparisonModel
 import com.app.ecarepro.ui.dashbord.model.BankBalanceModel
 import com.app.ecarepro.ui.dashbord.model.EstimateCollectionModel
 import com.app.ecarepro.ui.dashbord.model.FeeDefaulterModel
+import com.app.ecarepro.ui.dashbord.model.LibraryBookStatusModel
+import com.app.ecarepro.ui.dashbord.model.OnlineVsOfflineAdmissionModel
 import com.app.ecarepro.ui.dashbord.model.StaffAttendanceModel
+import com.app.ecarepro.ui.dashbord.model.StanderWiseStatisticModel
+import com.app.ecarepro.ui.dashbord.model.StudentStatisticModel
+import com.app.ecarepro.ui.dashbord.model.TeachersBirthdayCarouselModel
 import com.rubensousa.decorator.LinearMarginDecoration
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
@@ -77,6 +88,24 @@ class DashboardFragment : Fragment() {
             if (data.showStfAttendanceSummary == true)
                 buildStaffAttendanceCard(data.staffAttendanceSummary)
 
+            if (data.showAdmissionComparison == true)
+                buildAdmissionComparisonCard(data.admissionComparison)
+
+            if (data.showStuStatusWiseStatistics == true)
+                buildStanderWiseStatistic(data.stuStatusWiseStatistics)
+
+            if (data.showAdmissionModeComparison == true)
+                buildOnlineVsOfflineAdmissionCard(data.admissionModeComparison)
+
+            if (data.showStuCategoryStatistics == true)
+                buildStudentStatisticModel(data.admissionModeComparison)
+
+            if (data.showLibraryDTL == true)
+                buildLibraryBookStatusModel(data.libraryDTL)
+
+            if (data.showBDayCards == true)
+                buildTeachersBirthdayCarouselModel(data.birthDayCards)
+
 
         }
 
@@ -128,6 +157,59 @@ class DashboardFragment : Fragment() {
 
     }
 
+    private fun EpoxyController.buildAdmissionComparisonCard(admissionComparisonModel: AdmissionComparison?) {
+        admissionComparisonModel ?: return
+        AdmissionComparisonModel(admissionComparisonModel)
+            .id("1355")
+            .addTo(this)
+    }
+
+    private fun EpoxyController.buildStanderWiseStatistic(statusWiseStatistics: List<StatusWiseStatistics>?) {
+        if (statusWiseStatistics.isNullOrEmpty()) {
+            return
+        }
+        StanderWiseStatisticModel(statusWiseStatistics)
+            .id("1e355")
+            .addTo(this)
+    }
+
+    private fun EpoxyController.buildOnlineVsOfflineAdmissionCard(data: List<DataValue>?) {
+        if (data.isNullOrEmpty()) {
+            return
+        }
+        OnlineVsOfflineAdmissionModel(data)
+            .id("1e3e55")
+            .addTo(this)
+    }
+
+
+    private fun EpoxyController.buildStudentStatisticModel(data: List<DataValue>?) {
+        if (data.isNullOrEmpty()) {
+            return
+        }
+        StudentStatisticModel(data)
+            .id("1e5e36eerfr55")
+            .addTo(this)
+    }
+
+    private fun EpoxyController.buildLibraryBookStatusModel(data: LibraryDetails?) {
+        if (data == null) {
+            return
+        }
+        LibraryBookStatusModel(data)
+            .id("1ee36er55")
+            .addTo(this)
+    }
+
+    private fun EpoxyController.buildTeachersBirthdayCarouselModel(data: List<BirthDayCard>?) {
+        if (data == null) {
+            return
+        }
+        TeachersBirthdayCarouselModel(data)
+            .id("1e5e")
+            .addTo(this)
+    }
+
     private fun initViews() {
         binding.recyclerView.apply {
 
@@ -147,9 +229,6 @@ class DashboardFragment : Fragment() {
                      .id("135")
                      .addTo(this)
 
-                 AdmissionComparisonModel()
-                     .id("1355")
-                     .addTo(this)
 
                  StanderWiseStatisticModel()
                      .id("1e355")
--- app/src/main/java/com/app/ecarepro/ui/dashbord/model/OnlineVsOfflineAdmissionModel.kt
+++ app/src/main/java/com/app/ecarepro/ui/dashbord/model/OnlineVsOfflineAdmissionModel.kt
@@ -2,15 +2,14 @@ package com.app.ecarepro.ui.dashbord.model
 
 import androidx.core.view.isVisible
 import com.app.ecarepro.R
+import com.app.ecarepro.data.network.model.DataValue
 import com.app.ecarepro.databinding.ItemOnlineVsOfflineAdmissionCardBinding
-import com.app.ecarepro.databinding.ItemStaffAttendanceCardBinding
-import com.app.ecarepro.databinding.ItemStanderWiseStatisticCardBinding
 import com.app.ecarepro.ui.views.epoxy.ViewBindingKotlinModel
 import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
 import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
 import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
 
-class OnlineVsOfflineAdmissionModel :
+class OnlineVsOfflineAdmissionModel(val data: List<DataValue>) :
     ViewBindingKotlinModel<ItemOnlineVsOfflineAdmissionCardBinding>(R.layout.item_online_vs_offline_admission_card) {
     private var isExpanded = false
     override fun ItemOnlineVsOfflineAdmissionCardBinding.bind() {
@@ -20,11 +19,15 @@ class OnlineVsOfflineAdmissionModel :
             chartView.isVisible = this@OnlineVsOfflineAdmissionModel.isExpanded
             groupCollapsed.isVisible = this@OnlineVsOfflineAdmissionModel.isExpanded.not()
         }
+        lvOnline.setTitle(data.firstOrNull()?.data ?: "")
+        lvOnline.setSubTitle(data.firstOrNull()?.value.toString())
+        lvOffline.setTitle(data.getOrNull(1)?.data ?: "")
+        lvOffline.setSubTitle(data.getOrNull(1)?.value.toString())
 
-        chartView.aa_drawChartWithChartModel(getBarChartModel())
+        chartView.aa_drawChartWithChartModel(getBarChartModel(data))
     }
 
-    private fun getBarChartModel() = AAChartModel()
+    private fun getBarChartModel(data: List<DataValue>) = AAChartModel()
         .chartType(AAChartType.Pie)
         .dataLabelsEnabled(true) //是否直接显示扇形图数据
         .series(
@@ -36,10 +39,9 @@ class OnlineVsOfflineAdmissionModel :
                     .borderWidth(0) //描边的宽度
                     .allowPointSelect(false) //是否允许在点击数据点标记(扇形图点击选中的块发生位移)
                     .data(
-                        arrayOf(
-                           arrayOf("Online",20),
-                           arrayOf("Offline",80),
-                        )
+                        data.map {
+                            arrayOf(it.data, it.value)
+                        }.toTypedArray()
                     )
             )
         )
