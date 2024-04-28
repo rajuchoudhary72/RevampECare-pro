5aeaaa64e674f245564fa7fa35c73af699b4176d
Raju <raju.s@speedyy.com>
2024-04-28 11:26:59 +0530

    DashboardFragment total complete

--- app/src/main/java/com/app/ecarepro/ui/dashbord/model/TeachersBirthdayCarouselModel.kt
+++ app/src/main/java/com/app/ecarepro/ui/dashbord/model/TeachersBirthdayCarouselModel.kt
@@ -1,20 +1,20 @@
 package com.app.ecarepro.ui.dashbord.model
 
 import com.app.ecarepro.R
-import com.app.ecarepro.databinding.ItemRecentPhotoCarouselCardBinding
+import com.app.ecarepro.data.network.model.BirthDayCard
 import com.app.ecarepro.databinding.ItemTeachersBirthdayCarouselCardBinding
-import com.app.ecarepro.photoView
 import com.app.ecarepro.teacherBirthdayCard
 import com.app.ecarepro.ui.views.epoxy.ViewBindingKotlinModel
 
-class TeachersBirthdayCarouselModel :
+class TeachersBirthdayCarouselModel(val data: List<BirthDayCard>) :
     ViewBindingKotlinModel<ItemTeachersBirthdayCarouselCardBinding>(R.layout.item_teachers_birthday_carousel_card) {
     override fun ItemTeachersBirthdayCarouselCardBinding.bind() {
         carousel.numViewsToShowOnScreen = 1.8f
         carousel.withModels {
-            (0..5).forEach {
+            data.forEach {
                 teacherBirthdayCard{
-                    id(it)
+                    id(it.heading)
+                    data(it)
                 }
 
             }
