09b8abe86eb89166004521efa30b549d19cfe79c
Raju <raju.s@speedyy.com>
2024-06-14 15:58:53 +0530

    versionName = "1.1.3" release  on beta  version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.franciscan.ecare_pro"
         minSdk = 23
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.1.2"
+        versionCode = 5
+        versionName = "1.1.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
