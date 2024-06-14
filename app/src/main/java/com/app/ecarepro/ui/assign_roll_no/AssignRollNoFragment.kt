68a43a205d0f9003d4b1f49ab61430eeb9fae243
Gourav <gourav.bhati@franciscansolutions.net>
2024-06-14 13:41:00 +0530

    fix bugs till 14 june aain

--- app/src/main/java/com/app/ecarepro/ui/assign_roll_no/AssignRollNoFragment.kt
+++ app/src/main/java/com/app/ecarepro/ui/assign_roll_no/AssignRollNoFragment.kt
@@ -29,8 +29,7 @@ import com.app.ecarepro.model.MyClasseTeacherOf
 import com.app.ecarepro.model.StudentPro
 import com.app.ecarepro.model.StudentRllNo
 import com.app.ecarepro.ui.MainActivity
-import com.app.ecarepro.ui.mainActivity
-import com.app.ecarepro.utils.Constant
+ import com.app.ecarepro.utils.Constant
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -231,7 +230,7 @@ class AssignRollNoFragment : Fragment(), MenuProvider {
             requestList.add(AssignRollNoBodyItem(d.houseID,d.rollNumber,d.stID))
         }
         assignRollNoViewModel.assignRollNumber(requestList).invokeOnCompletion {
-            mainActivity().showMessage("Roll Number Assign Successfully")
+            Toast.makeText( requireContext() ,"Roll Number Assign Successfully",Toast.LENGTH_SHORT).show()
             menuHost.removeMenuProvider(this)
         }
 
