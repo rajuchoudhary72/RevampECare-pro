5b1ab8375817e59172ddc3710388df6c67616d8c
Raju <raju.s@speedyy.com>
2024-07-02 09:35:13 +0530

    versionName = "1.1.7"  release with final  all fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.franciscan.ecare_pro"
         minSdk = 23
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.1.6"
+        versionCode = 9
+        versionName = "1.1.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
