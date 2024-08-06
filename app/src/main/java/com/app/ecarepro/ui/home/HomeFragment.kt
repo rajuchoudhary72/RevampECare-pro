8ee8b7a676a6f01d217cf12657d8bff693357d7a
Raju <raju.s@speedyy.com>
2024-08-06 21:19:26 +0530

    bug fix and release  build  1.1.11

--- app/src/main/java/com/app/ecarepro/ui/home/HomeFragment.kt
+++ app/src/main/java/com/app/ecarepro/ui/home/HomeFragment.kt
@@ -82,7 +82,7 @@ class HomeFragment : Fragment() {
 
     private fun setUpViews() {
        // binding.btnMenu.setOnClickListener { systemViewModel.openDrawer(true) }
-       // binding.imgUserAvatar.setOnClickListener { findNavController().navigate(R.id.profileFragment) }
+        binding.imgUserAvatar.setOnClickListener { findNavController().navigate(R.id.profileFragment) }
         binding.recyclerView.addItemDecoration(
             LinearMarginDecoration.create(
                 margin = 8,
