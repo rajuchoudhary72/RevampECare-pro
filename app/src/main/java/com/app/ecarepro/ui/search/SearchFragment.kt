e6a8922c51f9d1547dbdb096da48b5ebee0a57da
Hari <hari.choudhary1905@gmail.com>
2024-08-06 13:44:49 +0530

    fav

--- app/src/main/java/com/app/ecarepro/ui/search/SearchFragment.kt
+++ app/src/main/java/com/app/ecarepro/ui/search/SearchFragment.kt
@@ -41,6 +41,11 @@ class SearchFragment : Fragment() {
         return _binding?.root
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        searchViewModel.clearSearchQuery()
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUpViews()
