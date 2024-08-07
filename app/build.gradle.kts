835259454a01b3ce98745e1f4815c1f5fb62b7c2
Raju <raju.s@speedyy.com>
2024-08-07 08:56:27 +0530

    bug fix #148496,#148967 ,#148509,#149154, #145437  fixed and release @gourav code with this  ticket #148805,#148830, #148860  release on this  versionName = "1.1.12"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.franciscan.ecare_pro"
         minSdk = 23
         targetSdk = 34
-        versionCode = 13
-        versionName = "1.1.11"
+        versionCode = 14
+        versionName = "1.1.12"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
