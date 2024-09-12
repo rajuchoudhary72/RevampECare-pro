9864281b9fc6810349f504ae815c437777ee18c6
Hari <hari.choudhary1905@gmail.com>
2024-09-12 20:07:31 +0530

    fav

--- app/src/main/java/com/app/ecarepro/data/network/model/UserDashboardDto.kt
+++ app/src/main/java/com/app/ecarepro/data/network/model/UserDashboardDto.kt
@@ -109,7 +109,10 @@ data class AttendanceSummary(
     val totalLeave: Int?,
     @SerializedName("totalPresent")
     val totalPresent: Int?
-) : Serializable
+) : Serializable {
+    fun totalStudent() =
+        (totalPresent ?: 0) + (totalAbsent ?: 0) + (totalLeave ?: 0) + (totalLate ?: 0)
+}
 
 data class BirthDayCard(
     @SerializedName("color")
