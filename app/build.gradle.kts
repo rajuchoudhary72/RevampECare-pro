0d6d048f822b65099cb85362c2d9acd2000de33c
Raju <raju.s@speedyy.com>
2024-09-18 21:03:05 +0530

    2.1.77 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.franciscan.ecare_pro"
         minSdk = 23
         targetSdk = 34
-        versionCode = 206
-        versionName = "2.1.76"
+        versionCode = 207
+        versionName = "2.1.77"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         //add this in the build.gradle.kts(app) file
         javaCompileOptions {
