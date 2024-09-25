619b37e3107dbc7172598e72527596859d795cf3
Raju <raju.s@speedyy.com>
2024-09-25 16:38:20 +0530

    migration  of multiple userID  issue fixed

--- app/src/main/java/com/app/ecarepro/ui/fee/FeePaymentFragment.kt
+++ app/src/main/java/com/app/ecarepro/ui/fee/FeePaymentFragment.kt
@@ -97,11 +97,11 @@ class FeePaymentFragment : Fragment() {
 
        Log.i("paymentUrl",feePaymentViewModel.feePayemtURL + "?token=" + tokenKey)
 
-
+// https://payment.lfconventschoolsangrur.com/mlogin.aspx
         val tabIntent = CustomTabsIntent.Builder()
             .setToolbarColor( (requireActivity() as MainActivity).getColor(R.color.green)).build()
         openCustomTab(tabIntent, Uri.parse(feePaymentViewModel.feePayemtURL + "?token=" + tokenKey))
-        binding.apply {
+      /*  binding.apply {
             (requireActivity() as MainActivity).showLoader(true)
             wvFeePayment.settings.javaScriptEnabled = true
             wvFeePayment.settings.setSupportZoom(true)
@@ -121,7 +121,7 @@ class FeePaymentFragment : Fragment() {
 
 
             wvFeePayment.loadUrl( feePaymentViewModel.feePayemtURL + "?token=" + tokenKey   )
-        }
+        }*/
 
 
 
