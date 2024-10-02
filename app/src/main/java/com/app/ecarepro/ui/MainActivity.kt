b52f24699882d367b871b8c7b1df70119b77354f
Raju <raju.s@speedyy.com>
2024-10-02 21:11:05 +0530

    fix crash of  calender  of attendance

--- app/src/main/java/com/app/ecarepro/ui/MainActivity.kt
+++ app/src/main/java/com/app/ecarepro/ui/MainActivity.kt
@@ -99,6 +99,7 @@ class MainActivity : AppCompatActivity() {
 
     @Inject
     lateinit var userDataStore: UserDataStore
+
     @Inject
     lateinit var userDatabase: UserDatabase
     private val topLevelFragments = mutableListOf(
@@ -352,60 +353,64 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-        private fun handleNotificationClick(data: Bundle) {
-            lifecycleScope.launch {
-                showLoader(true)
-                delay(2000)
-
-                Log.e("Note", data.keySet().joinToString() {key -> "$key -> ${data.get(key).toString()}"  } )
-                val schCode = data.getString("SchCode") ?: return@launch
-                val userID = data.getString("UserID")?.toInt() ?: return@launch
-                val userType = data.getString("UserType")?.toInt() ?: return@launch
-                val menuId = data.getString("MenuId")?.toInt()
-                val childMenuId = data.getString("ChMenuID")?.toInt()
-
-                Log.e("Note", "$schCode $userID $menuId $childMenuId" )
-
-                if(userDataStore.getUsersFlow().first().firstOrNull { it.userId == userID && it.schoolCode == schCode } == null){
-                    Log.e("Note", "return@launch", )
-                    return@launch
-                }
-
-                val currentSchool = userDataStore.getSchoolData()
-                if (currentSchool?.schoolCode != schCode) {
-                    Log.e("Note", "userDataStore.setCurrentSchoolCode(schCode)", )
-                    userDataStore.setCurrentSchoolCode(schCode!!)
-                }
-
-                val currentUser = userDataStore.getUser()
-                if (currentUser?.userId != userID) {
-                    Log.e("Note", "userDataStore.setCurrentUserId(userID)", )
-                            userDatabase.getUser(userID, schCode, userType)?.id?.let {
-                                userDataStore.setCurrentUserId(it)
-                            }
-                }
-
-                if (menuId != null) {
-                    if (childMenuId != null) {
-                        Log.e("Note","getFragmentId(menuId, childMenuId)", )
-                        getFragmentId(menuId, childMenuId)
-                    }
-                }
-                showLoader(false)
-            }
-
-        }
-
-/*
     private fun handleNotificationClick(data: Bundle) {
-        val menuId = data.getString("MenuId")?.toInt()
-        val childMenuId = data.getString("ChMenuID")?.toInt()
-        if (menuId != null) {
-            if (childMenuId != null) {
-                getFragmentId(menuId, childMenuId)
+        lifecycleScope.launch {
+            showLoader(true)
+            delay(2000)
+
+            Log.e(
+                "Note",
+                data.keySet().joinToString() { key -> "$key -> ${data.get(key).toString()}" })
+            val schCode = data.getString("SchCode") ?: return@launch
+            val userID = data.getString("UserID")?.toInt() ?: return@launch
+            val userType = data.getString("UserType")?.toInt() ?: return@launch
+            val menuId = data.getString("MenuId")?.toInt()
+            val childMenuId = data.getString("ChMenuID")?.toInt()
+
+            Log.e("Note", "$schCode $userID $menuId $childMenuId")
+
+            if (userDataStore.getUsersFlow().first()
+                    .firstOrNull { it.userId == userID && it.schoolCode == schCode } == null
+            ) {
+                Log.e("Note", "return@launch")
+                return@launch
             }
+
+            val currentSchool = userDataStore.getSchoolData()
+            if (currentSchool?.schoolCode != schCode) {
+                Log.e("Note", "userDataStore.setCurrentSchoolCode(schCode)")
+                userDataStore.setCurrentSchoolCode(schCode!!)
+            }
+
+            val currentUser = userDataStore.getUser()
+            if (currentUser?.userId != userID) {
+                Log.e("Note", "userDataStore.setCurrentUserId(userID)")
+                userDatabase.getUser(userID, schCode, userType)?.id?.let {
+                    userDataStore.setCurrentUserId(it)
+                }
+            }
+
+            if (menuId != null) {
+                if (childMenuId != null) {
+                    Log.e("Note", "getFragmentId(menuId, childMenuId)")
+                    getFragmentId(menuId, childMenuId)
+                }
+            }
+            showLoader(false)
         }
-    }*/
+
+    }
+
+    /*
+        private fun handleNotificationClick(data: Bundle) {
+            val menuId = data.getString("MenuId")?.toInt()
+            val childMenuId = data.getString("ChMenuID")?.toInt()
+            if (menuId != null) {
+                if (childMenuId != null) {
+                    getFragmentId(menuId, childMenuId)
+                }
+            }
+        }*/
 
     private fun checkAppVersion() {
         lifecycleScope.launch {
@@ -436,27 +441,33 @@ class MainActivity : AppCompatActivity() {
                             }
                             Log.v("okhttp", "versionCode $versionCode")
                             Log.v("okhttp", "versionName $versionName")
-
-                            if (versionName < it.data.android.currentVersion) {
-                                // open  dialog
-                                if (versionName > it.data.android.criticalVersion && it.data.android.normalVersion < versionName) {
-                                    //soft  update
-                                    UpdateAppVersionDialog(
-                                        0,
-                                        it.data.android.title,
-                                        it.data.android.description
-                                    )
-                                } else {
-                                    //force update
-                                    UpdateAppVersionDialog(
-                                        1,
-                                        it.data.android.title,
-                                        it.data.android.description
-                                    )
+                            try {
+                                if (it.data.android.currentVersion != null) {
+                                    if (versionName < it.data.android.currentVersion) {
+                                        // open  dialog
+                                        if (versionName > it.data.android.criticalVersion && it.data.android.normalVersion < versionName) {
+                                            //soft  update
+                                            UpdateAppVersionDialog(
+                                                0,
+                                                it.data.android.title,
+                                                it.data.android.description
+                                            )
+                                        } else {
+                                            //force update
+                                            UpdateAppVersionDialog(
+                                                1,
+                                                it.data.android.title,
+                                                it.data.android.description
+                                            )
+                                        }
+                                    } else {
+                                        // nothing  open  version  dialog
+                                    }
                                 }
-                            } else {
-                                // nothing  open  version  dialog
+                            } catch (e: NullPointerException) {
+                                e.message
                             }
+
 
                             /* if (versionCode < it.data.android.versionCode) {
 
@@ -494,6 +505,7 @@ class MainActivity : AppCompatActivity() {
             imm?.hideSoftInputFromWindow(view.windowToken, 0)
         }
     }
+
     fun setUpDrawer() {
         systemViewModel.openNavigationDrawer.observe(this) { open ->
             if (open) {
@@ -781,10 +793,11 @@ class MainActivity : AppCompatActivity() {
                             if (userType == Constant.STAFF_TYPE) {
                                 navController.navigate(R.id.attendanceFragment)
                             } else {
-                                val intent1 = Intent(this@MainActivity, TryAttendanceTest2::class.java)
+                                val intent1 =
+                                    Intent(this@MainActivity, TryAttendanceTest2::class.java)
                                 startActivity(intent1)
 
-                            //    navController.navigate(R.id.showAttendanceFragment)
+                                //    navController.navigate(R.id.showAttendanceFragment)
                             }
                         } catch (_: Exception) {
                         }
@@ -1061,7 +1074,7 @@ class MainActivity : AppCompatActivity() {
                     45 -> navController.navigate(R.id.staticalReport)
                     46 -> navController.navigate(R.id.appUserReportFragment)
                     47 -> navController.navigate(R.id.surveyListFragment)
-                    64 ->  {
+                    64 -> {
 
                         navController.navigate(
                             R.id.classAndTeacherListFragment,
@@ -1070,8 +1083,6 @@ class MainActivity : AppCompatActivity() {
                             })
 
                     }
-
-
 
 
                 }
@@ -1185,6 +1196,7 @@ class MainActivity : AppCompatActivity() {
                             3 -> {
                                 navController.navigate(R.id.assignHomeFragment)
                             }
+
                             10 -> {
                                 navController.navigate(R.id.updateStudentsProfileFragment)
                             }
