a9425e3a6bc6da6957bacfe141c66ef35f5db8d3
Raju <raju.s@speedyy.com>
2024-10-12 18:50:06 +0530

    avoid scientific notation and  sync  DB ans @gourav code merge and rlease  2.1.99

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.franciscan.ecare_pro"
         minSdk = 23
         targetSdk = 34
-        versionCode = 228
-        versionName = "2.1.98"
+        versionCode = 229
+        versionName = "2.1.99"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         //add this in the build.gradle.kts(app) file
         javaCompileOptions {
