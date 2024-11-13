34bfa22e490de0e024f2fc46fd14ef15f1ea11af
gouravbhati <gourav.bhati@franciscansolutions.net>
2024-11-13 18:24:22 +0530

    fix total students count

--- app/src/main/java/com/app/ecarepro/ui/report/StudentAttendanceSummeryFragment.kt
+++ app/src/main/java/com/app/ecarepro/ui/report/StudentAttendanceSummeryFragment.kt
@@ -316,7 +316,7 @@ class StudentAttendanceSummeryFragment : Fragment(), ItemListener<ClassSummary> 
         binding.pieChart.animateXY(1400, 1400)
 
         val s = """
-            ${totalPresent + totalAbsent + totalLeave + totalLate + totalNA}
+            ${totalPresent + totalAbsent + totalLeave  + totalNA}
             Student(s)
             """.trimIndent()
         val length = (totalPresent + totalAbsent + totalLeave).toString() + ""
