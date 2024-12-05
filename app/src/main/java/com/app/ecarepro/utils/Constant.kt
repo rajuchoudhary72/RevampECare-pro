6388c5f924f0d5e4f14c4c73b4c16ce78d04c382
Raju <raju.s@speedyy.com>
2024-12-05 20:23:50 +0530

    main branch  merge

--- app/src/main/java/com/app/ecarepro/utils/Constant.kt
+++ app/src/main/java/com/app/ecarepro/utils/Constant.kt
@@ -84,10 +84,13 @@ class Constant {
         const val CHECK_OUT = 5
         const val APPROVE = 3
         const val REJECT = 4
-
+        const val LEAVE_APPROVE = "Approved"
+        const val LEAVE_REJECT = "Rejected"
+        const val LEAVE_PENDING = "Pending"
         const val FROM = "from"
         const val LEAVE_ACTION_APPROVE = 1
         const val LEAVE_ACTION_REJECT = 2
+        const val LEAVE_ACTION_CANCEL = -1
 
         const val PROFILE_FRA_STU = "ProfileFragmentStudent"
         const val PROFILE_FRA_STAFF = "ProfileFragmentStaff"
