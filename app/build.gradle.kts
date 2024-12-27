8669c139af09df76b8c8cc4e08327bb85e626f8f
Raju <raju.s@speedyy.com>
2024-12-27 17:55:06 +0530

    release  #159953 #154165

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.franciscan.ecare_pro"
         minSdk = 23
         targetSdk = 34
-        versionCode = 272
-        versionName = "2.6.1"
+        versionCode = 273
+        versionName = "2.6.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         //add this in the build.gradle.kts(app) file
         javaCompileOptions {
--- app/src/main/java/com/app/ecarepro/utils/Constant.kt
+++ app/src/main/java/com/app/ecarepro/utils/Constant.kt
@@ -22,6 +22,7 @@ class Constant {
 
         /*new  base Url*/
          const val BASE_URL = "https://androidapi.franciscanecare.net/"
+         const val REPORT_BASE_URL = "https://pay.franciscanwebsolutions.com/"
         const val BASE_DEV_URL = "https://apiuat.franciscanecare.net/"
         const val BASE_URL_COM = "https://app.franciscanecare.com"
         const val AUTH_BEFORE_LOGIN_NEW = "Kq4IYAuSXLh4EsnexoTSfA=="
