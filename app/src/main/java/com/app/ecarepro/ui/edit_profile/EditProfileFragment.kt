d8b46252ffe999248b285622b928dfc7df72bfac
gouravbhati <gourav.bhati@franciscansolutions.net>
2025-01-06 17:18:41 +0530

    implement teacher list , minor change in edit profile

--- app/src/main/java/com/app/ecarepro/ui/edit_profile/EditProfileFragment.kt
+++ app/src/main/java/com/app/ecarepro/ui/edit_profile/EditProfileFragment.kt
@@ -202,7 +202,7 @@ binding.cbSameAddress.setOnCheckedChangeListener {
             parentStatus.setText(profile.studentProfile.parentStaus, false)
 
             //Child's Details
-
+            textStudentName.setText(profile.studentProfile.name)
             textStudentEmail.setText(profile.studentProfile.studentEmail)
             textClass.setText(profile.studentProfile.className)
             textDOB.setText(profile.studentProfile.dob)
