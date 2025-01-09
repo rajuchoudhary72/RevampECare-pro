0088736b9486d0782386f80b3909209cdba300dc
Raju <raju.s@speedyy.com>
2025-01-09 10:59:25 +0530

    PerformanceMonitorInterceptor done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         applicationId = "com.franciscan.ecare_pro"
         minSdk = 23
         targetSdk = 34
-        versionCode = 279
-        versionName = "2.6.7"
+        versionCode = 280
+        versionName = "2.6.8"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         //add this in the build.gradle.kts(app) file
         javaCompileOptions {
--- app/src/main/java/com/app/ecarepro/di/NetworkModule.kt
+++ app/src/main/java/com/app/ecarepro/di/NetworkModule.kt
@@ -59,8 +59,6 @@ object NetworkModule {
             .writeTimeout(60, TimeUnit.SECONDS)
             .build()
     }
-/* .addInterceptor(connectivityInterceptor)
-            .addInterceptor(customResponseInterceptor)*/
 @Provides
 fun provideRetrofit(
     okHttpClient: OkHttpClient,
