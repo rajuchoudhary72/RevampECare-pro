f333a1db72863d882f5942dbd9f1f208654e5a5e
Raju <raju.s@speedyy.com>
2025-01-11 18:01:11 +0530

    error code  handling

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.franciscan.ecare_pro"
         minSdk = 23
         targetSdk = 34
-        versionCode = 280
-        versionName = "2.6.8"
+        versionCode = 281
+        versionName = "2.6.9"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         //add this in the build.gradle.kts(app) file
         javaCompileOptions {
