0a6eaec029f0dee5a324d6867dba86508f8c835b
gouravbhati <gourav.bhati@franciscansolutions.net>
2025-01-18 21:48:24 +0530

    fee receipt download issue fixed

--- app/src/main/java/com/app/ecarepro/ui/fee/fee_receipt/FeeReceiptFragment.kt
+++ app/src/main/java/com/app/ecarepro/ui/fee/fee_receipt/FeeReceiptFragment.kt
@@ -339,11 +339,12 @@ class FeeReceiptFragment : Fragment() , ItemListener <FeeReceipt> {
 
         try {
             val decodedBytes = Base64.decode(base64String, Base64.DEFAULT)
+            val currentTime = System.currentTimeMillis()
 
 
            // val file = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), "FeeReceipt_"+"$recdate.pdf")
             val file = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
-                "FeeReceipt$recdate.pdf"
+                "FeeReceipt$recdate$currentTime.pdf"
             )
             try {
 
