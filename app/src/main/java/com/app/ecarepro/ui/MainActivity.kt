02720cc55444fbdaf475d979c3ec614ed2c310fa
gouravbhati <gourav.bhati@franciscansolutions.net>
2025-01-22 12:57:26 +0530

    22 jan work

--- app/src/main/java/com/app/ecarepro/ui/MainActivity.kt
+++ app/src/main/java/com/app/ecarepro/ui/MainActivity.kt
@@ -1066,7 +1066,6 @@ class MainActivity : AppCompatActivity() {
             showMessage("Invalid or missing URL")
             return
         }
-
         if (isChromeInstalled(this)) {
             val packageName = "com.android.chrome"
             customTabsIntent.intent.setPackage(packageName)
