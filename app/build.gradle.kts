57f7cd816b5fffd4b6a0934cd0a2e65dc4f3018c
Raju <raju.s@speedyy.com>
2025-01-24 22:27:03 +0530

    merged user session code in main  branch  and live in internal build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.franciscan.ecare_pro"
         minSdk = 23
         targetSdk = 34
-        versionCode = 291
-        versionName = "2.7.9"
+        versionCode = 292
+        versionName = "2.8.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         //add this in the build.gradle.kts(app) file
         javaCompileOptions {
