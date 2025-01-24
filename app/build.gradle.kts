7f9a50665adb54d68993e12093a6c848f200ee82
Raju <raju.s@speedyy.com>
2025-01-24 22:12:37 +0530

    final code merged in main branch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.franciscan.ecare_pro"
         minSdk = 23
         targetSdk = 34
-        versionCode = 290
-        versionName = "2.7.8"
+        versionCode = 291
+        versionName = "2.7.9"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         //add this in the build.gradle.kts(app) file
         javaCompileOptions {
