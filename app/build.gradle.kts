63b4f2ed6c98c122e359fd6de5b798c5baf60396
Raju <raju.s@speedyy.com>
2025-01-29 18:46:42 +0530

    jks added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.franciscan.ecare_pro"
         minSdk = 23
         targetSdk = 34
-        versionCode = 294
-        versionName = "2.8.2"
+        versionCode = 295
+        versionName = "2.8.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         //add this in the build.gradle.kts(app) file
         javaCompileOptions {
