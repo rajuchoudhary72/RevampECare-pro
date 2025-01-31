71e1a2ac9823a4a9e1e903774e5a832409000593
Raju <raju.s@speedyy.com>
2025-01-31 21:29:58 +0530

    release 2.8.7 with   remove retry code  from errorCode=1 code .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.franciscan.ecare_pro"
         minSdk = 23
         targetSdk = 34
-        versionCode = 297
-        versionName = "2.8.5"
+        versionCode = 299
+        versionName = "2.8.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         //add this in the build.gradle.kts(app) file
         javaCompileOptions {
--- app/src/main/java/com/app/ecarepro/ui/lessonPlan/view/ViewLessonPlanFragment.kt
+++ app/src/main/java/com/app/ecarepro/ui/lessonPlan/view/ViewLessonPlanFragment.kt
@@ -92,48 +92,37 @@ class ViewLessonPlanFragment : Fragment() {
                                     append(" by ")
                                     append(it.data.lessonPlans.actionTakenBy)
                                 }
-
                                 if (it.data.lessonPlans.status==0){
                                     binding. tvStatus.setTextColor( this@ViewLessonPlanFragment.resources.getColor(R.color.att_late_color,null))
                                     binding. tvStatus.text="Pending"
-
                                 }else{
                                     binding. tvStatus.setTextColor( this@ViewLessonPlanFragment.resources.getColor(R.color.green,null))
                                     binding. tvStatus.text="Approved"
-
                                 }
-                                if (it.data.lessonPlans.attachment.fileURL!=null){
-                                    binding.llFile.setOnClickListener { _ ->
-                                        downloadFile(it.data.lessonPlans.attachment.fileURL)
+                                try {
+                                    if (it.data.lessonPlans.attachment!=null){
+                                        if (it.data.lessonPlans.attachment.fileURL !=null){
+                                            binding.llFile.setOnClickListener { _ ->
+                                                downloadFile(it.data.lessonPlans.attachment.fileURL)
+                                            }
+                                        }else{
+                                            binding.llFile.isVisible=false
+                                        }
+                                    }else{
+                                        binding.llFile.isVisible=false
                                     }
-                                }else{
-                                    binding.llFile.isVisible=false
+                                }catch (e:NullPointerException){
+                                    e.printStackTrace()
                                 }
-
                                 binding.cvDetailsApprove.isVisible = it.data.lessonPlans.status != 1
-
-
-
-
                             }
-
                         }
-
                     }
-
 
                 }
             }
         }
-
-
-
         viewLessonPlanViewModel.getLessonPlanDTL( id, teacherID)
-
-
-
-
-
     }
 
     private fun downloadFile(fileSource:String){
