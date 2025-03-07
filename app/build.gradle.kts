bb393b0b67be408f92ae7ed06e971c244c0b85dd
Raju <raju.s@speedyy.com>
2025-03-07 09:34:21 +0530

    urgent  issue  fixed  and  release .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.franciscan.ecare_pro"
         minSdk = 23
         targetSdk = 34
-        versionCode = 314
-        versionName = "3.0.2"
+        versionCode = 315
+        versionName = "3.0.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         //add this in the build.gradle.kts(app) file
         javaCompileOptions {
