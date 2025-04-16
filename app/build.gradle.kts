5cf0550bd63444a38cb536dd28475ed433fd0f91
Raju <raju.s@speedyy.com>
2025-04-16 13:02:11 +0530

    merge minor code of  gourav and  release  of 3.1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.franciscan.ecare_pro"
         minSdk = 23
         targetSdk = 34
-        versionCode = 325
-        versionName = "3.1.4"
+        versionCode = 327
+        versionName = "3.1.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         //add this in the build.gradle.kts(app) file
         javaCompileOptions {
