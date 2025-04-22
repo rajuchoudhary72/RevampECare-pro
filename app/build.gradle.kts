ecf2a43c1b8aade158047fddfaca47aac16fc236
Raju <raju.s@speedyy.com>
2025-04-22 10:27:53 +0530

    3.1.8 need to release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.franciscan.ecare_pro"
         minSdk = 23
         targetSdk = 34
-        versionCode = 328
-        versionName = "3.1.7"
+        versionCode = 329
+        versionName = "3.1.8"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         //add this in the build.gradle.kts(app) file
         javaCompileOptions {
