4be8af33004e86b2515849dc94318a5cf3385936
gouravbhati <gourav.bhati@franciscansolutions.net>
2025-04-30 16:11:59 +0530

    remove localation

--- app/src/main/java/com/app/ecarepro/di/NetworkModule.kt
+++ app/src/main/java/com/app/ecarepro/di/NetworkModule.kt
@@ -26,9 +26,6 @@ import java.util.concurrent.TimeUnit
 import com.app.ecarepro.di.annotations.SessionReCreate
 import com.app.ecarepro.data.network.InvalidSessionInterceptor
 import com.app.ecarepro.data.network.SessionAuthenticator
-import com.app.ecarepro.data.network.service.GoogleSheetsApiService
-import javax.inject.Singleton
-
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
@@ -130,11 +127,6 @@ fun provideRetrofit(
         return retrofit.create(FomApiService::class.java)
     }
 
-    @Provides
-    @Singleton
-    fun provideGoogleSheetsApiService(retrofit: Retrofit): GoogleSheetsApiService {
-        return retrofit.create(GoogleSheetsApiService::class.java)
-    }
 
     @Provides
     @SessionReCreate
--- app/src/main/java/com/app/ecarepro/ui/language/dynamic_language/SheetsApiResponseModel.kt
+++
@@ -1,5 +0,0 @@
-package com.app.ecarepro.ui.language.dynamic_language
-
-data class SheetsApiResponseModel(
-    val values: List<List<String>>
-)
