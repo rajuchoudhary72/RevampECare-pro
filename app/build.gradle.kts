c43468c595880e31625923b5232ff75dcfb414e8
gouravbhati <gourav.bhati@franciscansolutions.net>
2025-05-27 13:57:51 +0530

    27 may work

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -201,7 +201,7 @@ dependencies {
     implementation("com.github.chrisbanes:PhotoView:2.3.0")
     implementation("com.intuit.sdp:sdp-android:1.0.5")
     implementation("de.hdodenhof:circleimageview:2.2.0")
-    implementation("com.github.Mindinventory:Lassi:1.3.0")
+    implementation("com.github.Mindinventory:Lassi:1.4.2")
     implementation("com.github.AsynctaskCoffee:VoiceRecorder:beta-0.5")
     implementation("com.github.dhaval2404:imagepicker:2.1")
     implementation("com.github.PhilJay:MPAndroidChart:v3.0.3")
