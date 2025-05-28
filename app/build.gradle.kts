7e7989bc64c2819db3e5d847a9b82b5f5f030ed2
Raju <raju.s@speedyy.com>
2025-05-28 13:18:59 +0530

    Release build with  3.3.3  and  Gourav code merge

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.franciscan.ecare_pro"
         minSdk = 23
         targetSdk = 34
-        versionCode = 343
-        versionName = "3.3.2"
+        versionCode = 344
+        versionName = "3.3.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         //add this in the build.gradle.kts(app) file
         javaCompileOptions {
