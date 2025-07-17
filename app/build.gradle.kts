ad720f0faf66a98993c20681e371993790f3d388
Raju <raju.s@speedyy.com>
2025-07-17 12:03:44 +0530

    #181930 Done and app release  3.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.franciscan.ecare_pro"
         minSdk = 23
         targetSdk = 34
-        versionCode = 351
-        versionName = "3.4.0"
+        versionCode = 352
+        versionName = "3.4.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         //add this in the build.gradle.kts(app) file
         javaCompileOptions {
