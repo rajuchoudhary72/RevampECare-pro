44c79ccc03e64aa81ab9dbdaa8cac6c93ae554f0
gouravbhati <gourav.bhati@franciscansolutions.net>
2025-07-25 13:50:39 +0530

    languge loc merge new code

--- app/src/main/java/com/app/ecarepro/ui/lessonPlan/view/ViewLessonPlanFragment.kt
+++ app/src/main/java/com/app/ecarepro/ui/lessonPlan/view/ViewLessonPlanFragment.kt
@@ -1,11 +1,18 @@
 package com.app.ecarepro.ui.lessonPlan.view
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.TextView
+import android.widget.Toast
+import android.util.Base64
 import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
@@ -16,11 +23,14 @@ import com.app.ecarepro.AddMoreFavouritesBindingModelBuilder
 import com.app.ecarepro.R
 import com.app.ecarepro.data.network.model.NetworkResult
 import com.app.ecarepro.databinding.FragmentViewLessonPlanBinding
+import com.app.ecarepro.model.Dtl
 import com.app.ecarepro.ui.MainActivity
 import com.app.ecarepro.ui.lessonPlan.LessonPlanListAdapter
+import com.app.ecarepro.ui.mainActivity
 import com.app.ecarepro.ui.photoview.PhotoViewFragmentFragment
 import com.app.ecarepro.utils.AndroidDownloader
 import com.app.ecarepro.utils.Constant
+import com.google.android.material.textfield.TextInputEditText
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.collectLatest
@@ -30,31 +40,103 @@ import kotlinx.coroutines.launch
 @AndroidEntryPoint
 class ViewLessonPlanFragment : Fragment() {
 
-
-    private var lPlanId: String= ""
-    private lateinit var binding : FragmentViewLessonPlanBinding
-    private val viewLessonPlanViewModel : ViewLessonPlanViewModel by viewModels()
+    private var lPlanId: String = ""
+    private var lPlanPush: String = ""
+    private var lPlanIdnew: Int = 0
+    private lateinit var binding: FragmentViewLessonPlanBinding
+    private val viewLessonPlanViewModel: ViewLessonPlanViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View  {
-        binding= FragmentViewLessonPlanBinding.inflate(inflater,container,false)
+    ): View {
+        binding = FragmentViewLessonPlanBinding.inflate(inflater, container, false)
         lPlanId = requireArguments().getString(Constant.LESSON_ID_ARGUMENT).toString()
+        lPlanPush = requireArguments().getString(Constant.LESSONPLAN_HARDCCODE_KEY ).toString()
         binding.toolbar.setNavigationOnClickListener { findNavController().popBackStack() }
+
+        if (Constant.LESSONPLAN_HARDCCODE_KEY.isEmpty()) {
+            binding.llCheck.isVisible = false
+            binding.llAction.isVisible = false
+        } else {
+            binding.llCheck.isVisible = false
+            binding.llAction.isVisible = true
+        }
+        binding.tvReject.setOnClickListener {
+            popUpRemark(true)
+        }
+        binding.tvApprove.setOnClickListener {
+            popUpRemark(false)
+        }
         return binding.root
     }
 
+    private fun popUpRemark(optionalReason: Boolean) {
+        val tv_done: TextView
+        val tv_cancel: TextView
+        val tv_heading: TextView
+        val alertmsg: TextView
+        val textInputEditText: TextInputEditText
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+
+        val dialog = Dialog(requireContext())
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        if (null != dialog.window) dialog.window!!.setBackgroundDrawable(
+            ColorDrawable(Color.TRANSPARENT)
+        )
+        dialog.window!!.attributes.windowAnimations = R.style.Animations
+        dialog.setContentView(R.layout.custom_popup_leave_reject)
+        alertmsg = dialog.findViewById(R.id.alertmsg)
+        tv_heading = dialog.findViewById(R.id.tv_heading)
+        tv_done = dialog.findViewById(R.id.tv_done)
+        tv_cancel = dialog.findViewById(R.id.tv_cancel)
+        textInputEditText = dialog.findViewById(R.id.textFiledReason)
+        if (optionalReason) {
+            tv_done.setBackgroundColor(resources.getColor(R.color.red))
+            tv_heading.text = getString(R.string.reject_plan)
+            alertmsg.text = getString(R.string.are_you_sure_you_want_to_reject_the_lesson_plan)
+        }else{
+            tv_done.text = getString(R.string.approve)
+            tv_done.setBackgroundColor(resources.getColor(R.color.green))
+            tv_heading.text = getString(R.string.approve_plan)
+            alertmsg.text = getString(R.string.are_you_sure_you_want_to_approve_the_lesson_plan)
+        }
 
 
 
-        getLessonPlanDTL(lPlanId,0)
+        tv_done.setOnClickListener {
+            val reason: String = textInputEditText.text.toString().trim()
+
+            if (optionalReason) {
+                if (textInputEditText.text.toString().isNotEmpty()) {
+                    //do reject  code here
+                    viewLessonPlanViewModel.lessonPlanAction(lPlanIdnew, 2, reason).invokeOnCompletion {
+                        getLessonPlanDTL(lPlanId, 0)
+                    }
+                    dialog.dismiss()
+                } else {
+                    Toast.makeText(requireContext(),"Rejection Reason is mandatory field",Toast.LENGTH_SHORT).show()
+                    // textInputEditText.error = "Rejection Reason is mandatory field"
+                }
+            } else {
+                //do approve code here
+
+                viewLessonPlanViewModel.lessonPlanAction(lPlanIdnew, 1, reason).invokeOnCompletion {
+                    getLessonPlanDTL(lPlanId, 0)
+                }
+                dialog.dismiss()
+            }
+
+        }
+        tv_cancel.setOnClickListener { dialog.dismiss() }
+        dialog.show()
 
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        getLessonPlanDTL(lPlanId, 0)
+    }
 
 
     private fun getLessonPlanDTL(
@@ -71,7 +153,7 @@ class ViewLessonPlanFragment : Fragment() {
 
                     is NetworkResult.Error -> {
                         (requireActivity() as MainActivity).showLoader(false)
-                        Log.e("ytututut",it.message.toString())
+                        Log.e("ytututut", it.message.toString())
                     }
 
                     is NetworkResult.Success -> {
@@ -79,53 +161,109 @@ class ViewLessonPlanFragment : Fragment() {
                         if (it.data != null) {
 
                             if (it.data.lessonPlans != null) {
+                                lPlanIdnew= it.data.lessonPlans.lPlnID
 
-                                binding.lessonData=it.data.lessonPlans
+                                binding.lessonData = it.data.lessonPlans
 
-                                binding.tvPlanDuration.text= buildString {
+                                binding.tvPlanDuration.text = buildString {
                                     append(it.data.lessonPlans.fromDate)
-                                    append(get(R.string._to_))
+                                    append(" to ")
                                     append(it.data.lessonPlans.tillDate)
                                 }
 
-                                binding.tvStatusBy.text= buildString {
-                                    append(get(R.string._by_))
+                                binding.tvStatusBy.text = buildString {
+                                    append(" by ")
                                     append(it.data.lessonPlans.actionTakenBy)
                                 }
-                                if (it.data.lessonPlans.status==0){
-                                    binding. tvStatus.setTextColor( this@ViewLessonPlanFragment.resources.getColor(R.color.att_late_color,null))
-                                    binding. tvStatus.text=getString(R.string.pending)
-                                }else{
-                                    binding. tvStatus.setTextColor( this@ViewLessonPlanFragment.resources.getColor(R.color.green,null))
-                                    binding. tvStatus.text=getString(R.string.approved)
+                                if (it.data.lessonPlans.status == 0) {
+                                    binding.tvStatus.setTextColor(
+                                        this@ViewLessonPlanFragment.resources.getColor(
+                                            R.color.att_late_color,
+                                            null
+                                        )
+                                    )
+                                    binding.tvStatus.text = "Pending"
+                                } else  if (it.data.lessonPlans.status == 2) {
+                                    binding.tvStatus.setTextColor(
+                                        this@ViewLessonPlanFragment.resources.getColor(
+                                            R.color.red,
+                                            null
+                                        )
+                                    )
+                                    binding.tvStatus.text = "Rejected"
+                                } else {
+                                    binding.tvStatus.setTextColor(
+                                        this@ViewLessonPlanFragment.resources.getColor(
+                                            R.color.green,
+                                            null
+                                        )
+                                    )
+                                    binding.tvStatus.text = "Approved"
                                 }
                                 try {
-                                    if (it.data.lessonPlans.attachment!=null){
-                                        if (it.data.lessonPlans.attachment.fileURL !=null){
-                                            binding.llFile.setOnClickListener { _ ->
+                                    if (it.data.lessonPlans.attachment != null) {
+                                        if (it.data.lessonPlans.attachment.fileURL != null) {
+                                            binding.llDownload.setOnClickListener { _ ->
                                                 downloadFile(it.data.lessonPlans.attachment.fileURL)
                                             }
-                                        }else{
-                                            binding.llFile.isVisible=false
+                                            binding.llView.setOnClickListener { _ ->
+                                                openFile(it.data.lessonPlans.attachment.fileURL)
+                                            }
+                                        } else {
+                                            binding.llFile.isVisible = false
                                         }
-                                    }else{
-                                        binding.llFile.isVisible=false
+                                    } else {
+                                        binding.llFile.isVisible = false
                                     }
-                                }catch (e:NullPointerException){
+                                } catch (e: NullPointerException) {
                                     e.printStackTrace()
                                 }
-                                binding.cvDetailsApprove.isVisible = it.data.lessonPlans.status != 1
+                                if (it.data.lessonPlans.status==0){
+                                    binding.cvDetailsApprove.isVisible = it.data.lessonPlans.status ==0
+                                }else{
+                                    binding.cvDetailsApprove.isVisible = false
+                                }
                             }
                         }
                     }
 
+
                 }
             }
         }
-        viewLessonPlanViewModel.getLessonPlanDTL( id, teacherID)
+        viewLessonPlanViewModel.getLessonPlanDTL(id, teacherID)
     }
 
-    private fun downloadFile(fileSource:String){
+    private fun openFile(fileSource: String) {
+        when (Constant.isPdfUrl(fileSource)){
+            1 -> {
+                findNavController().navigate(R.id.openPdfFragment, Bundle().apply {
+                    putString(Constant.URL_ARGUMENT, fileSource)
+                })
+            }
+            2 -> {
+                findNavController().navigate(
+                    R.id.photoViewFragmentFragment,
+                    bundleOf(PhotoViewFragmentFragment.PHOTO to fileSource)
+                )
+            }
+            3 -> {
+                findNavController().navigate(R.id.openPdfFragment, Bundle().apply {
+                    putString(Constant.URL_ARGUMENT, fileSource)
+                })
+            }else -> {
+            findNavController().navigate(
+                R.id.photoViewFragmentFragment,
+                bundleOf(PhotoViewFragmentFragment.PHOTO to fileSource)
+            )
+        }
+        }
+
+
+
+    }
+
+    private fun downloadFile(fileSource: String) {
         try {
             when (Constant.isPdfUrl(fileSource)) {
 
@@ -136,32 +274,28 @@ class ViewLessonPlanFragment : Fragment() {
 
                 2 -> {
                     val androidDownloader = AndroidDownloader(requireContext())
-                    androidDownloader.downloadFile(fileSource,
-                        getString(R.string.photo), "image/jpeg")
+                    androidDownloader.downloadFile(fileSource, "Photo", "image/jpeg")
                 }
 
                 3 -> {
                     val androidDownloader = AndroidDownloader(requireContext())
-                    androidDownloader.downloadFile(fileSource, getString(R.string.lesson),"application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+                    androidDownloader.downloadFile(
+                        fileSource,
+                        getString(R.string.lesson),
+                        "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+                    )
 
                 }
 
                 else -> {
                     val androidDownloader = AndroidDownloader(requireContext())
-                    androidDownloader.downloadFile(fileSource, getString(R.string.photo), "image/jpeg")
+                    androidDownloader.downloadFile(fileSource, "Photo", "image/jpeg")
                 }
             }
-        }catch (e:NullPointerException){
+        } catch (e: NullPointerException) {
             e.printStackTrace()
         }
 
 
     }
-    }
-
-
-
-
-
-
-
+}
--- app/src/main/java/com/app/ecarepro/ui/transport_attendance/view/TransportAttendanceReportFragment.kt
+++ app/src/main/java/com/app/ecarepro/ui/transport_attendance/view/TransportAttendanceReportFragment.kt
@@ -40,7 +40,7 @@ class TransportAttendanceReportFragment : Fragment() {
     private val transportAttReportViewModel: TransportAttReportViewModel by viewModels()
 
     private lateinit var routeLSTList: List<RouteLST>
-    private lateinit var stopLSTList: List<StopLST>
+    private  var stopLSTList = mutableListOf<StopLST>()
     private lateinit var stoppersSelectData: StopLST
     private var routeSelected: Boolean = false
     private var stoppersSelected: Boolean = false
@@ -80,10 +80,14 @@ class TransportAttendanceReportFragment : Fragment() {
 
             }
             tvSelectStoppage.setOnClickListener {
-                if (stopLSTList.isNotEmpty()) {
-                    popUpStoppers()
-                } else {
-                    mainActivity().showMessage(getString(R.string.no_stoppers_data))
+                if (routeSelected) {
+                    if (stopLSTList.isNotEmpty()) {
+                        popUpStoppers()
+                    } else {
+                        mainActivity().showMessage("No Stoppers Data")
+                    }
+                }else{
+                    mainActivity().showMessage("No Stoppers Data")
                 }
             }
 
@@ -134,7 +138,7 @@ class TransportAttendanceReportFragment : Fragment() {
                     is NetworkResult.Success -> {
                         (requireActivity() as MainActivity).showLoader(false)
                         if (it.data != null) {
-                            stopLSTList = it.data.stopLST
+                            stopLSTList = it.data.stopLST.toMutableList()
                         }
                     }
                 }
