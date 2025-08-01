31017512fab2660271d3d05afcac89246be5c486
Raju <raju.s@speedyy.com>
2025-08-01 15:01:53 +0530

    Release

--- app/src/main/java/com/app/ecarepro/utils/Constant.kt
+++ app/src/main/java/com/app/ecarepro/utils/Constant.kt
@@ -22,6 +22,7 @@ class Constant {
 
         /*new  base Url*/
          //const val BASE_URL = "https://androidapi.franciscanecare.net/"
+
          const val BASE_URL = "https://api.franciscanecare.net/"
          const val REPORT_BASE_URL = "https://pay.franciscanwebsolutions.com/"
         const val BASE_DEV_URL = "https://apiuat.franciscanecare.net/"
