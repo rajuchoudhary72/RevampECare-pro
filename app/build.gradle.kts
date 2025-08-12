872f4a54fd852e27b74d82eb3638d6d2fcc5ef30
Raju <raju.s@speedyy.com>
2025-08-12 14:30:27 +0530

    final  Language and copy paste issue . fully

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.franciscan.ecare_pro"
         minSdk = 23
         targetSdk = 34
-        versionCode = 362
-        versionName = "3.5.1"
+        versionCode = 363
+        versionName = "3.5.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         //add this in the build.gradle.kts(app) file
         javaCompileOptions {
