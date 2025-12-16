2dfe1d0bfbbb66b892d0400844115fb987315398
Raju <raju.s@speedyy.com>
2025-12-16 16:50:11 +0530

    latest code

--- app/src/main/java/com/app/ecarepro/MainActivity.kt
+++ app/src/main/java/com/app/ecarepro/MainActivity.kt
@@ -12,8 +12,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
-        // Turn off the decor fitting system windows, which allows us to handle insets,
-        // including IME animations
         super.onCreate(savedInstanceState)
         setContent {
             EcareProTheme {
