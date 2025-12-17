42aa87bd5a8dbe847228112f062bb356d9f6bf42
Raju <raju.s@speedyy.com>
2025-12-17 12:25:27 +0530

    latest code

--- app/src/main/java/com/app/ecarepro/MainActivity.kt
+++ app/src/main/java/com/app/ecarepro/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+
         setContent {
             EcareProTheme {
                 ECareProApp()
