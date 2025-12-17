19234d067b41cf3469dd4c578095cbe97698c001
rajuchoudhary72 <rajuchoudharay72@gmail.com>
2025-12-17 12:34:36 +0530

    latest code

--- app/src/main/java/com/app/ecarepro/MainActivity.kt
+++ app/src/main/java/com/app/ecarepro/MainActivity.kt
@@ -12,7 +12,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
-
         super.onCreate(savedInstanceState)
         setContent {
             EcareProTheme {
